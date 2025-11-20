0e64196bb9878a9cea4ace794d3f39e180b1cd1b
SagarBhausahebSahane <sbsahane57@gmail.com>
2025-11-20 09:15:56 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,8 @@ app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
+
+console.log(process.env.FRONTEND_URL);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
